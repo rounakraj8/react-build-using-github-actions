a0838ca727d56c9e6465d1b49059b57b06adc79e
rounakraj8 <rounakraj8@gmail.com>
2020-07-25 00:19:20 +0530

    add data in data.js file

--- react-app/src/Steps.js
+++ react-app/src/Steps.js
@@ -1,42 +1,20 @@
 import React from 'react';
 import Step from './Step';
+import { steps } from './config/data.js';
+
 
 class Steps extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            steps: [
-                {
-                    name: "Step 1",
-                    header: 'We need to use GitHub actions to build the artifact as soon as we push our code to GitHub master branch.',
-                    subSteps: [
-                        "Create a React App",
-                        "Push the code to GitHub",
-                    ]
-                
-                },
-                {
-                    name: "Step 2",
-                    header: 'We need to use GitHub actions to build the artifact as soon as we push our code to GitHub master branch.',
-                    subSteps: [
-                    ]
-                }
-            ]
-        }
-    }
-
     render() {
-
         return (
             <div>
-                {this.state.steps.map((step, key) =>
-
+                {steps.map((step, key) =>
                     <Step key={key} step={step} />
                 )}
             </div>
         );
     }
-
 }
+
+
 
 export default Steps;
--- react-app/src/config/data.js
+++ react-app/src/config/data.js
@@ -1,10 +1,17 @@
-export const StepsData = {
-    Step1: {
-        header: 'We need to use GitHub actions to build the artifact as soon as we push our code to GitHub master branch.'
-    },
-    Step2: {
-        header: 'We need to use GitHub actions to build the artifact as soon as we push our code to GitHub master branch.'
-    }
-};
+export const steps = [
+    {
+        name: "Step 1",
+        header: 'We need to use GitHub actions to build the artifact as soon as we push our code to GitHub master branch.',
+        subSteps: [
+            "Create a React App",
+            "Push the code to GitHub",
+        ]
 
-export default StepsData;
+    },
+    {
+        name: "Step 2",
+        header: 'We need to use GitHub actions to build the artifact as soon as we push our code to GitHub master branch.',
+        subSteps: [
+        ]
+    }
+];
